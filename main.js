b299fff9c4227ef4ddd1d92acb549b53d3aa5ef8
reaganiwadha <reaganiwadha@gmail.com>
2018-08-19 13:30:42 +0700

    added bootstrap

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = require('electron');
 let mainWindow
 
 function createWindow(){
-    mainWindow = new BrowserWindow({width:800,height:800,frame:false});
+    mainWindow = new BrowserWindow({width:800,height:600,frame:false});
     mainWindow.loadFile('index.html')
     mainWindow.on('closed',function(){
         mainWindow = null
