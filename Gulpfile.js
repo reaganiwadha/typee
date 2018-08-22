735b2dee0a78e9fad57155f24d7ac7fce603df33
Fransiskus Febryan <franfebryan@gmail.com>
2018-08-22 18:17:35 +0700

    Replace grunt with gulp, add Gulpfile.js

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+const gulp     = require("gulp");
+const sass     = require("gulp-sass");
+const sassLint = require("gulp-sass-lint");
+
+gulp.task("build-css", function(){
+    return gulp.src("sass/*.scss")
+      .pipe(sass().on("error", sass.logError))
+      .pipe(gulp.dest("./css/"));
+});
+
+gulp.task("lint-scss", function(){
+    return gulp.src("scss/*.scss")
+      .pipe(sassLint())
+      .pipe(sassLint.format())
+      .pipe(sassLint.failOnError());
+});
+
+gulp.task("build", ["lint-scss", "build-css"])
+
+gulp.task("watch-scss", function(){
+    gulp.watch("scss/*.scss", ["build-css", "lint-scss"]);
+});
