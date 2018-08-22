d381d367b35605f5c97f5ecb8fc6fc4b786de85e
Fransiskus Febryan <franfebryan@gmail.com>
2018-08-22 18:49:23 +0700

    Fix typo

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,9 @@ const sass     = require("gulp-sass");
 const sassLint = require("gulp-sass-lint");
 
 gulp.task("build-css", function(){
-    return gulp.src("sass/*.scss")
+    return gulp.src("scss/*.scss")
       .pipe(sass().on("error", sass.logError))
-      .pipe(gulp.dest("./css/"));
+      .pipe(gulp.dest("css/"));
 });
 
 gulp.task("lint-scss", function(){
