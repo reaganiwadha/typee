27de64e00c0b345990969867d22b4636d3dd93b5
reaganiwadha <reaganiwadha@gmail.com>
2018-08-23 18:42:02 +0700

    Replace with better solutions

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,25 @@
 const remote = require('electron').remote;
 
-function kill(){
+function kill() {
     remote.getCurrentWindow().close();
 }
 
 $(".close.button").on("click", kill);
 
+$(document.getElementById("buttonContainer").children).removeClass('keatas');
 
-function buttonime(){
+function buttonime() {
     var bc = document.getElementById("buttonContainer").children;
     bc[0].classList.add('keatas');
-    for(var i=1; i<bc.length; i++){
-        setTimeout(function(i){
-            bc[i].classList.add('keatas');
-        },150*i,i);
+    for (var i = 1; i < bc.length; i++) {
+        $(bc[i]).attr("style","animation-delay: "+150*i+"ms;");
+        bc[i].classList.add('keatas');    
     }
 }
 
-function startWbwm(){
+//define buttons
+$('#wbwm').on("click", startWbwm);
+
+function startWbwm() {
     buttonime();
 }
