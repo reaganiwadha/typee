5d4ac5caa7fab44f387a9b5d3f28927b15508dc0
reaganiwadha <reaganiwadha@gmail.com>
2018-08-23 16:56:25 +0700

    Animation on the menu when clicked

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,18 @@ function kill(){
 }
 
 $(".close.button").on("click", kill);
+
+
+function buttonime(){
+    var bc = document.getElementById("buttonContainer").children;
+    bc[0].classList.add('keatas');
+    for(var i=1; i<bc.length; i++){
+        setTimeout(function(i){
+            bc[i].classList.add('keatas');
+        },150*i,i);
+    }
+}
+
+function startWbwm(){
+    buttonime();
+}
